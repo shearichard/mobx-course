ec452c273be827b9dbc74dc224e4e6e422872435
Richard Shea <rshea@thecubagroup.com>
2020-09-21 10:22:08 +1200

    Remainder of the reaction section

--- src/test.ts
+++ src/test.ts
@@ -1,18 +1,34 @@
-import {action, autorun, observable , runInAction } from "mobx";
+import {action, autorun, observable , runInAction, when, reaction } from "mobx";
 
 
 class Person {
     @observable
     firstName: string;
-
     @observable
     lastName: string;
+    @observable
+    age: number = 15;
+    @observable
+    isAlive: boolean = true;
     //
-    constructor(name: string, lastName: string){
-        this.firstName = name;
-        this.lastName = lastName;
+    constructor(props: Partial<Person>){
+        Object.assign(this, props);
+
+        when(
+            // once ...
+            () => this.age > 99,
+            // ... then                
+            () => this.bury(),
+        )
     }
-    
+    @action
+    bury() {
+        this.isAlive = false;
+    }
+    @action 
+    setAge(age: number){
+       this.age = age;
+    }
     @action 
     updateFirstName(name: string){
         this.firstName = name;
@@ -21,7 +37,6 @@ class Person {
     updateLastName(name: string){
         this.lastName = name;
     }
-
     @action 
     updateFullName(name: string, lastName: string){
         this.firstName = name;
@@ -29,7 +44,7 @@ class Person {
     }
 }
 
-const newPerson = new Person('Fred', 'Smith');
+const newPerson = new Person({firstName: 'Fred', lastName: 'Smith'});
 
 autorun(async () => {
     //-------------------------------------------------------------------------------
@@ -50,12 +65,34 @@ autorun(async () => {
     //and, when the values of `newPerson` are updated the new values are seen
     //in the log output
     //
-    console.log(`Person name is : ${newPerson.firstName} ${newPerson.lastName}`);
+    console.log(`Person name is : ${newPerson.firstName} ${newPerson.lastName} (${newPerson.age}) ${newPerson.isAlive}`);
 });
+//
+//
+const disposer = reaction(
+    //once ...
+    () => newPerson.isAlive === false,
+    //... then
+    () => console.log("RIP")
+);
 //
 runInAction(async () => {
     newPerson.firstName = 'Martha';
     newPerson.lastName = 'Jones';
 });
+
+newPerson.setAge(100);
+
+//Note that having run once in response
+//to setting the age to 100 the reaction
+//does not run again as age is set to 90
+//and then back to 100.
+newPerson.setAge(90);
+newPerson.setAge(100);
+
+//The function returned from 'reaction'
+//deactivate the reaction and returns
+//the memory
+disposer();
 
 export {};
